2a4022d9a6be0b451dcefafe0785c2b671222afe
Todd Whitelow <todd@whitelow.tech>
2023-07-30 09:29:29 -0600

    remove strict mode and remove the uneeded items

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
-import { Header, List } from 'semantic-ui-react';
+import { Header, List, Button } from 'semantic-ui-react';
 
 function App() {
   const [activities, setActivities] = useState([]);
@@ -10,7 +10,6 @@ function App() {
   useEffect(() => {
     axios.get('http://localhost:5000/api/activities')
       .then(response => {
-        console.log(response);
         setActivities(response.data);
       })
   }, [])
